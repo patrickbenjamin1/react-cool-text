10a65247dfb8241600457e058e8e2191a9adeb54
patrickbenjamin1 <crumbleshake@live.co.uk>
2020-02-03 17:14:40 +0000

    fixed export default

--- react-cool-text/source/index.tsx
+++ react-cool-text/source/index.tsx
@@ -1,4 +1,5 @@
 export { CoolLetter, ICoolLetterProps } from './components/letter';
 export { CoolText, ICoolTextProps } from './components/text';
 
+import { CoolText } from './components/text';
 export default CoolText;
